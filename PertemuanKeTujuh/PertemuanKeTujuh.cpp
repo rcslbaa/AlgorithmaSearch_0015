5184732184c299fc76c1a8b4552257d46f03bbf7
rcslbaa <ifasmarani3125@gmail.com>
2024-04-04 18:01:51 +0700

    commit 16

--- PertemuanKeTujuh/PertemuanKeTujuh.cpp
+++ PertemuanKeTujuh/PertemuanKeTujuh.cpp
@@ -61,3 +61,12 @@ void LinearSearch()
 		cin >> ch;
 
 	} while ((ch == 'y') || (ch == 'Y'));
+}
+
+void display()
+{
+	cout << "Array elements are: ";
+	for (i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
