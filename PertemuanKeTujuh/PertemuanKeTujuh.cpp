52741f5f81040340b395e55d2dceff9af676a5eb
rcslbaa <ifasmarani3125@gmail.com>
2024-04-04 17:57:27 +0700

    commit 8

--- PertemuanKeTujuh/PertemuanKeTujuh.cpp
+++ PertemuanKeTujuh/PertemuanKeTujuh.cpp
@@ -33,3 +33,9 @@ void LinearSearch()
 	char ch;
 	int ctr; //Number of comparisons
 	int item;
+
+	do
+	{
+		// Accept the number to be searched
+		cout << "\nEnter the element you want to search: "; // step 1
+		cin >> item;
