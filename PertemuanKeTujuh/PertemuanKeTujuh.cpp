7285c08f00b5f65639097586caf0997d06fceefa
rcslbaa <ifasmarani3125@gmail.com>
2024-04-04 17:52:04 +0700

    commit 2

--- PertemuanKeTujuh/PertemuanKeTujuh.cpp
+++ PertemuanKeTujuh/PertemuanKeTujuh.cpp
@@ -4,3 +4,11 @@ using namespace std;
 int arr[20]; // Array to be search
 int n;       // Number of elements in the array
 int i;       // Index of array element
+
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of elements in the array: ";
+		cin >> n;
+		
